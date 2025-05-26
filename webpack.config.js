675c88fa64e70d4c20ff58df960c36565a085b08
Sneha Sharma <snehav2109@gmail.com>
2025-05-26 18:42:48 +0530

    add configuration files

---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+const path = require('path');
+const CopyPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+  entry: {
+    popup: './src/popup/index.js',
+    background: './src/background/background.js'
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].js',
+    clean: true
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react']
+          }
+        }
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      }
+    ]
+  },
+  plugins: [
+    new CopyPlugin({
+      patterns: [
+        { from: 'src/manifest.json', to: 'manifest.json' },
+        { from: 'src/popup/popup.html', to: 'popup.html' },
+        { from: 'icons', to: 'icons', noErrorOnMissing: true }
+      ]
+    })
+  ],
+  resolve: {
+    extensions: ['.js', '.jsx']
+  }
+};
